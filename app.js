8ab89bb8de735e3d20b288e7bc0607269c643530
Dima <yaremado@gmail.com>
2017-01-08 20:50:21 +0200

    Worked partwith http

--- app.js
+++ app.js
@@ -4,36 +4,44 @@
 angular.module('NarrowItDownApp', [])
 .controller('NarrowItDownController', NarrowItDownController)
 .service('MenuSearchService', MenuSearchService)
-.constant('ApiBasePath', " https://davids-restaurant.herokuapp.com/menu_items.json")
+.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com/menu_items.json");
 
 
 NarrowItDownController.$ingect = ['MenuSearchService'];
 function NarrowItDownController(MenuSearchService){
   var menu = this;
 
-  var promises = MenuSearchService.getMatchedMenuItems(searchTerm);
+  var promises = MenuSearchService.getMatchedMenuItems("Soup");
   promises.then(function (response){
-    menu.foundItem = response.data;
-  })
+    console.log(response);
+    menu.foundItem = response;})
   .catch(function (error) {
     console.log("Something went terribly wrong.");
   });
-
-
 }
 
 MenuSearchService.$ingect = ['$http','ApiBasePath'];
-function MenuSearchService($http,ApiBasePath){
+function MenuSearchService($http, ApiBasePath) {
   var service = this;
 
-  service.getMatchedMenuItems = function(searchTerm){
+  service.getMatchedMenuItems = function (searchTerm) {
     var response = $http({
       method: "GET",
-      ulr:ApiBasePath
+      url: ApiBasePath
+    }).then(function(result){
+      var foundItem = result.data;
+      var items = [];
+      for (var item in foundItem.menu_items ){
+        if (foundItem.menu_items[item].name.indexOf(searchTerm) !== -1) items.push(foundItem.menu_items[item]);
+      };
+      return items;
+    })
+    .catch(function(error){
+      console.log("Something wrong in Service.");
     });
-    return response;
-  };
 
+   return response;
+  };
 }
 
 } )();
